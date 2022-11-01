aec8fb34ed44171f86fabe914b896d880bdb786d
Jim Angelo Beren <jimangelo.beren@infor.com>
2022-11-01 21:11:55 +0800

    plugin: add colorizer plugin

--- config.lua
+++ config.lua
@@ -5,6 +5,7 @@ reload("user.autocommands")
 reload("user.lsp")
 reload("user.treesitter")
 
+reload("user.colorizer")
 reload("user.cybu")
 reload("user.nvim-tree")
 reload("user.harpoon")
@@ -15,4 +16,7 @@ reload("user.whichkey")
 reload("user.zen")
 
 
+-- TODO
+-- 1. Delete cmd-tabnine in the runtime folder
+-- 2. On treesitter on spellchecker
 
