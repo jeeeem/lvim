82bb3fe7fd273362222717c7c2ff840417988f6a
Jim Beren <jim.beren07@gmail.com>
2022-11-17 12:32:34 +0800

    chore: fix comments

--- config.lua
+++ config.lua
@@ -16,9 +16,10 @@ reload("user.todo")
 reload("user.whichkey")
 reload("user.zen")
 
-
 -- TODO:
 -- 1. Delete cmd-tabnine in the runtime folder
--- 2. On treesitter on spellchecker
+-- 2. Treesitter on spellchecker
 -- 3. Configure which-keys
 -- 4. Configure icons for todo-comments
+
+
