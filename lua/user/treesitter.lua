c6931819a233d4aa0a2a232c1c97880f56a67d7e
Jim Angelo Beren <jim.beren07@gmail.com>
2022-11-19 15:56:21 +0800

    feat(TS): update default TS

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,7 +1,9 @@
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "c",
+  -- "c",
   "lua",
+  "java",
+  "python"
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
