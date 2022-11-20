90d33b11f8be5c27f752074c7c9d648261d125af
Jim Angelo Beren <jim.beren07@gmail.com>
2022-11-20 23:22:52 +0800

    feat(plugins): add notify config ^& centered noice cmdline

--- lua/user/winsep.lua
+++ lua/user/winsep.lua
@@ -23,7 +23,7 @@ winsep.setup({
     local win_n = require("colorful-winsep.utils").getWinNumber()
     local win_id = vim.fn.win_getid(vim.fn.winnr('h'))
     local filetype = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(win_id), 'filetype')
-    if win_n == 2 or filetype == "NvimTree" then
+    if win_n == 2 then
       winsep.NvimSeparatorDel()
     end
   end,
