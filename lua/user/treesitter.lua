6b32fb88f57a5d55015f527225ebad93326647e5
Jim Angelo Beren <jim.beren07@gmail.com>
2022-11-20 20:12:14 +0800

    feat(TS): add powershell parser

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -8,3 +8,14 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
+
+-- Custom parsers
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.powershell = {
+  install_info = {
+    url = "https://github.com/jrsconfitto/tree-sitter-powershell",
+    files = {"src/parser.c"}
+  },
+  filetype = "ps1",
+  used_by = { "psm1", "psd1", "pssc", "psxml", "cdxml" }
+}
