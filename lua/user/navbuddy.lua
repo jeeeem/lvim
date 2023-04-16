9ce646232ac516050a16ef18deb27f8dd2702d84
Jim Angelo Beren <jim.beren07@gmail.com>
2023-04-16 16:13:18 +0800

    update: bump plugins

--- lua/user/navbuddy.lua
+++ lua/user/navbuddy.lua
@@ -63,47 +63,37 @@ navbuddy.setup {
     Operator      = " ",
     TypeParameter = " ",
   },
-  use_default_mappings = true,          -- If set to false, only mappings set
-  -- by user are set. Else default
-  -- mappings are used for keys
+  -- If set to false, only mappings set by user are set. Else default mappings are used for keys
   -- that are not set by user
+  use_default_mappings = true,
   mappings = {
     ["<esc>"] = actions.close,        -- Close and cursor to original location
     ["q"] = actions.close,
-
     ["j"] = actions.next_sibling,     -- down
     ["k"] = actions.previous_sibling, -- up
-
     ["h"] = actions.parent,           -- Move to left panel
     ["l"] = actions.children,         -- Move to right panel
     ["0"] = actions.root,             -- Move to first panel
-
     ["v"] = actions.visual_name,      -- Visual selection of name
     ["V"] = actions.visual_scope,     -- Visual selection of scope
-
     ["y"] = actions.yank_name,        -- Yank the name to system clipboard "+
     ["Y"] = actions.yank_scope,       -- Yank the scope to system clipboard "+
-
     ["i"] = actions.insert_name,      -- Insert at start of name
     ["I"] = actions.insert_scope,     -- Insert at start of scope
-
     ["a"] = actions.append_name,      -- Insert at end of name
     ["A"] = actions.append_scope,     -- Insert at end of scope
-
     ["r"] = actions.rename,           -- Rename currently focused symbol
-
     ["d"] = actions.delete,           -- Delete scope
-
     ["f"] = actions.fold_create,      -- Create fold of current scope
     ["F"] = actions.fold_delete,      -- Delete fold of current scope
-
     ["c"] = actions.comment,          -- Comment out current scope
-
     ["<enter>"] = actions.select,     -- Goto selected symbol
     ["o"] = actions.select,
-
     ["J"] = actions.move_down,        -- Move focused node down
     ["K"] = actions.move_up,          -- Move focused node up
+    ["t"] = actions.telescope({       -- Fuzzy finder at current level.
+      theme = "dropdown",
+    })
   },
   lsp = {
     auto_attach = true,   -- If set to true, you don't need to manually use attach function
