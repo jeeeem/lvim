5613de2eecc10489f992c435c6078ed198b1d191
Jim Angelo Beren <jim.beren07@gmail.com>
2023-04-24 10:27:58 +0800

    update(theme): add transparency

--- lua/user/theme/init.lua
+++ lua/user/theme/init.lua
@@ -1,8 +1,46 @@
--- Change theme settings
--- lvim.builtin.theme.options.dim_inactive = true
--- lvim.builtin.theme.options.style = "storm"
+local ok, transparent = pcall(require, "transparent")
 
--- https://github.com/EdenEast/nightfox.nvim
--- https://github.com/projekt0n/github-nvim-theme
--- https://github.com/catppuccin/nvim
--- https://github.com/Mofiqul/vscode.nvim
+if not ok then
+  return
+end
+
+lvim.colorscheme = "tokyonight"
+vim.g.tokyonight_transparent = vim.g.transparent_enabled
+
+transparent.setup({
+  groups = { -- table: default groups
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLineNr', 'EndOfBuffer', 'NormalSB', 'Pmenu'
+  },
+  extra_groups = { -- table: additional groups that should be cleared
+    "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+    'NvimTreeWinSeparator',
+    'NvimTreeNormal',
+    'TroubleNormal',
+    'TelescopeNormal',
+    'WhichKeyFloat',
+
+    -- TODO: programmatically add this
+    'NotifyINFOBody',
+    'NotifyERRORBody',
+    'NotifyWARNBody',
+    'NotifyDEBUGBody',
+    'NotifyTRACEBody',
+  },
+  exclude_groups = {}, -- table: groups you don't want to clear
+})
+
+vim.g.transparent_groups = vim.list_extend(
+  vim.g.transparent_groups or {},
+  vim.tbl_map(function(v)
+    return v.hl_group
+  end, vim.tbl_values(require('bufferline.config').highlights))
+)
+
+-- Possible colorschemes
+  -- https://github.com/EdenEast/nightfox.nvim
+  -- https://github.com/projekt0n/github-nvim-theme
+  -- https://github.com/catppuccin/nvim
+  -- https://github.com/Mofiqul/vscode.nvim
