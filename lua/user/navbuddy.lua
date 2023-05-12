815b2d4740751ca50c55c57f163c2f8f70ef9327
Jim Angelo Beren <jim.beren07@gmail.com>
2023-05-12 15:44:29 +0800

    update(plugin): fix navbuddy config

--- lua/user/navbuddy.lua
+++ lua/user/navbuddy.lua
@@ -35,6 +35,14 @@ navbuddy.setup {
       }
     }
   },
+  node_markers = {
+    enabled = true,
+    icons = {
+      leaf = "  ",
+      leaf_selected = " → ",
+      branch = " ",
+    },
+  },
   icons = {
     File          = " ",
     Module        = " ",
@@ -67,33 +75,34 @@ navbuddy.setup {
   -- that are not set by user
   use_default_mappings = true,
   mappings = {
-    ["<esc>"] = actions.close,        -- Close and cursor to original location
-    ["q"] = actions.close,
-    ["j"] = actions.next_sibling,     -- down
-    ["k"] = actions.previous_sibling, -- up
-    ["h"] = actions.parent,           -- Move to left panel
-    ["l"] = actions.children,         -- Move to right panel
-    ["0"] = actions.root,             -- Move to first panel
-    ["v"] = actions.visual_name,      -- Visual selection of name
-    ["V"] = actions.visual_scope,     -- Visual selection of scope
-    ["y"] = actions.yank_name,        -- Yank the name to system clipboard "+
-    ["Y"] = actions.yank_scope,       -- Yank the scope to system clipboard "+
-    ["i"] = actions.insert_name,      -- Insert at start of name
-    ["I"] = actions.insert_scope,     -- Insert at start of scope
-    ["a"] = actions.append_name,      -- Insert at end of name
-    ["A"] = actions.append_scope,     -- Insert at end of scope
-    ["r"] = actions.rename,           -- Rename currently focused symbol
-    ["d"] = actions.delete,           -- Delete scope
-    ["f"] = actions.fold_create,      -- Create fold of current scope
-    ["F"] = actions.fold_delete,      -- Delete fold of current scope
-    ["c"] = actions.comment,          -- Comment out current scope
-    ["<enter>"] = actions.select,     -- Goto selected symbol
-    ["o"] = actions.select,
-    ["J"] = actions.move_down,        -- Move focused node down
-    ["K"] = actions.move_up,          -- Move focused node up
+    ["<esc>"] = actions.close(),        -- Close and cursor to original location
+    ["q"] = actions.close(),
+    ["j"] = actions.next_sibling(),     -- down
+    ["k"] = actions.previous_sibling(), -- up
+    ["h"] = actions.parent(),           -- Move to left panel
+    ["l"] = actions.children(),         -- Move to right panel
+    ["0"] = actions.root(),             -- Move to first panel
+    ["v"] = actions.visual_name(),      -- Visual selection of name
+    ["V"] = actions.visual_scope(),     -- Visual selection of scope
+    ["y"] = actions.yank_name(),        -- Yank the name to system clipboard "+
+    ["Y"] = actions.yank_scope(),       -- Yank the scope to system clipboard "+
+    ["i"] = actions.insert_name(),      -- Insert at start of name
+    ["I"] = actions.insert_scope(),     -- Insert at start of scope
+    ["a"] = actions.append_name(),      -- Insert at end of name
+    ["A"] = actions.append_scope(),     -- Insert at end of scope
+    ["r"] = actions.rename(),           -- Rename currently focused symbol
+    ["d"] = actions.delete(),           -- Delete scope
+    ["f"] = actions.fold_create(),      -- Create fold of current scope
+    ["F"] = actions.fold_delete(),      -- Delete fold of current scope
+    ["c"] = actions.comment(),          -- Comment out current scope
+    ["<enter>"] = actions.select(),     -- Goto selected symbol
+    ["o"] = actions.select(),
+    ["J"] = actions.move_down(),        -- Move focused node down
+    ["K"] = actions.move_up(),          -- Move focused node up
     ["t"] = actions.telescope({       -- Fuzzy finder at current level.
       theme = "dropdown",
-    })
+    }),
+   ["g?"] = actions.help(),            -- Open mappings help window
   },
   lsp = {
     auto_attach = true,   -- If set to true, you don't need to manually use attach function
